d04c9bfbfa7065ee352e47ed7183ffcb3e08bb11
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-02 14:56:31 +0200

    Linking views in code

--- app/src/main/java/ws/tilda/anastasia/mvpapp/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/mvpapp/MainActivity.java
@@ -2,15 +2,21 @@ package ws.tilda.anastasia.mvpapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText mEditText;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        mEditText = findViewById(R.id.editText);
+        mButton = findViewById(R.id.button);
+
+
     }
 }
