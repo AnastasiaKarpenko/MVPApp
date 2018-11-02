06ba36a94d47c37e74b40ccdd51b6e3e527982bf
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-02 15:01:31 +0200

    Making Presenter class implement MainPresenter interface Making Constructor method that gets MainView in it

--- app/src/main/java/ws/tilda/anastasia/mvpapp/MainPresenter.java
+++ app/src/main/java/ws/tilda/anastasia/mvpapp/MainPresenter.java
@@ -1,4 +1,10 @@
 package ws.tilda.anastasia.mvpapp;
 
-public class MainPresenter {
+public class MainPresenter implements MainContractor.MainPresenter {
+
+    MainContractor.MainView mView;
+
+    public MainPresenter(MainContractor.MainView view) {
+        mView = view;
+    }
 }
